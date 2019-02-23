21b722d76a2a616b118b93793c0b5070f6da8097
victoriak94 <vkamila6@gmail.com>
2019-02-22 21:06:28 -0500

    Write formatCount function. Destructure repeated code inside function. Render function inside render method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,16 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+
+    return count === 0 ? "Zero" : count;
   }
 }
 

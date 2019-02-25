3169d897d88f9235f6eab536ee1bb6190135ee7d
victoriak94 <vkamila6@gmail.com>
2019-02-25 17:40:02 -0500

    Render conditional statement using && operator

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -22,6 +22,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
       </div>
     );
